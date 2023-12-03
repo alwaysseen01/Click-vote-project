7b80f9de7860d82bd233897f461350d6d8532bd6
Igor Ruzhilov <iruzhilov@gmail.com>
2023-12-03 21:53:20 +0600

    Configure basic project structure

---
+++ src/main/java/com/alwaysseen/clickvote/ClickvoteApplication.java
@@ -0,0 +1,13 @@
+package com.alwaysseen.clickvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alwaysseen/clickvote/ClickvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwaysseen.clickvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
